bc1ac68c1072b8c005e20201a4b6bd7b4f4c836a
Cris <22254235+00000o1@users.noreply.github.com>
2023-07-01 21:29:55 +0800

    40GB/s

--- newdisco.cpp
+++ newdisco.cpp
@@ -17,52 +17,45 @@ inline uint64_t rotl64(uint64_t x, int8_t r) {
     return (x << r) | (x >> (64 - r));
 }
 
-void process_chunk(const uint8_t* data, int len, unsigned seed, uint64_t& intermediate_hash) {
-    uint64_t hash = seed + PRIME_MULTIPLIER;
-
-    int num_blocks = len / 8;
-    for (int i = 0; i < num_blocks; i++) {
-        uint64_t block;
-        memcpy(&block, data + i * 8, 8);
-
-        hash ^= block;
-        hash = rotl64(hash, 13);
-        hash = hash * PRIME_MULTIPLIER + 0x9E3779B97F4A7C15ULL;
-    }
-
-    int offset = num_blocks * 8;
-    for (int i = 0; i < len - offset; i++) {
-        hash ^= data[offset + i];
-        hash = rotl64(hash, 5);
-        hash = hash * PRIME_MULTIPLIER + 0xBF58476D1CE4E5B9ULL;
-    }
-
-    intermediate_hash ^= hash;
-}
-
-
 void newdisco_64(const void* key, int len, unsigned seed, void* out) {
     const uint8_t* data = reinterpret_cast<const uint8_t*>(key);
 
     uint64_t hash = seed + PRIME_MULTIPLIER;
 
-    #pragma omp parallel reduction(^: hash)
-    {
-        uint64_t thread_hash = 0;
-        #pragma omp for
-        for (int i = 0; i < len; i += 8) {
+    // When the input has enough strength to conjure the spirits of OpenMP!
+    if (len >= 256) {
+        #pragma omp parallel for reduction(^: hash)
+        for (int i = 0; i < len / 8; i++) {
             uint64_t block;
-            memcpy(&block, data + i, 8);
+            memcpy(&block, data + i * 8, 8);
 
-            thread_hash ^= block;
-            thread_hash = rotl64(thread_hash, 13);
-            thread_hash = thread_hash * PRIME_MULTIPLIER + 0x9E3779B97F4A7C15ULL;
+            hash ^= block;
+            hash = rotl64(hash, 13);
+            hash = hash * PRIME_MULTIPLIER + 0x9E3779B97F4A7C15ULL;
         }
+    } else {
+        // For the whispers of small input, a lone warrior is dispatched!
+        for (int i = 0; i < len / 8; i++) {
+            uint64_t block;
+            memcpy(&block, data + i * 8, 8);
 
-        hash ^= thread_hash;
+            hash ^= block;
+            hash = rotl64(hash, 13);
+            hash = hash * PRIME_MULTIPLIER + 0x9E3779B97F4A7C15ULL;
+        }
     }
 
-    // Finalization
+    // Brave remainders, your valor shall not go unnoticed!
+    uint64_t remainder_hash = 0;
+    int offset = (len / 8) * 8;
+    for (int i = 0; i < len - offset; i++) {
+        remainder_hash ^= data[offset + i];
+        remainder_hash = rotl64(remainder_hash, 5);
+        remainder_hash = remainder_hash * PRIME_MULTIPLIER + 0xBF58476D1CE4E5B9ULL;
+    }
+    hash ^= remainder_hash;
+
+    // And thus the magic is sealed!
     hash ^= len;
     hash ^= (hash >> 33);
     hash *= 0xff51afd7ed558ccdULL;
@@ -72,4 +65,5 @@ void newdisco_64(const void* key, int len, unsigned seed, void* out) {
 
     memcpy(out, &hash, sizeof(hash));
 }
+
 
