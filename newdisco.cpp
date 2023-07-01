8476d2b47f561c2997392eb4616cfbf314c83733
Cris <22254235+00000o1@users.noreply.github.com>
2023-07-01 19:20:12 +0800

    Iteration with quality

--- newdisco.cpp
+++ newdisco.cpp
@@ -1,217 +1,49 @@
-// also known as BEBB4185
-// Copyright 2020 Cris Stringfellow
-// https://github.com/dosyago/discohash
-#include <cstdio>
-#include <inttypes.h>
+#include <cstdint>
+#include <cstdlib>
 #include <cstring>
-#include <iostream>
-#include "discohash.h"
+#include <algorithm>
 
-constexpr int STATE = 64;
-constexpr int STATEM = STATE-1;
-constexpr int STATE64 = STATE >> 3;
-constexpr int HSTATE64 = STATE64 >> 1;
-constexpr int STATE64M = STATE64-1;
-constexpr int HSTATE64M = HSTATE64-1;
-alignas(uint64_t) uint8_t ndisco_c_buf[STATE] = {0};
-constexpr uint64_t P = 0xFFFFFFFFFFFFFFFF - 58;
-constexpr uint64_t Q = 13166748625691186689U;
-alignas(uint64_t) uint8_t *ndcs8 = (uint8_t *)ndisco_c_buf;
-uint64_t *ndcs = (uint64_t *)ndisco_c_buf;
+constexpr uint64_t PRIME_MULTIPLIER = 0x880355f21e6d1965ULL;
 
-  static inline uint64_t rot( uint64_t v, int n) 
-  {
-    n = n & 63U;
-    if (n)
-        v = (v >> n) | (v << (64-n));
-    return v; 
-  }
+inline uint64_t rotl64(uint64_t x, int8_t r) {
+    return (x << r) | (x >> (64 - r));
+}
 
-  static inline uint8_t rot8( uint8_t v, int n) 
-  {
-    n = n & 7U;
-    if (n)
-        v = (v >> n) | (v << (8-n));
-    return v; 
-  }
+void newdisco_64(const void* key, int len, unsigned seed, void* out) {
+    const uint8_t* data = reinterpret_cast<const uint8_t*>(key);
+    uint64_t hash = seed + PRIME_MULTIPLIER;
 
-  /* 
-  // iteration 1
-  static inline void mix(const int A)
-  {
-      // mixing two 64-bit values
-      uint64_t x = ndcs[A];
-      uint64_t y = ndcs[A + 1];
+    // Allocate memory for alignment
+    alignas(uint64_t) uint8_t aligned_data[len];
+    memcpy(aligned_data, data, len);
 
-      // mixing x with y, and y with x
-      x ^= rot(y, 13);
-      y ^= rot(x, 25);
-      x ^= rot(y, 27);
-
-      // multiplications to avoid linearity, and bitwise-AND to wrap results
-      x = (x * Q) & 0xFFFFFFFFFFFFFFFF;
-      y = (y * P) & 0xFFFFFFFFFFFFFFFF;
-
-      // Storing back mixed values
-      ndcs[A] = x;
-      ndcs[A + 1] = y;
-  }
-  */
-
-  /* 
-  // iteration 2
-  static inline void mix(const int A)
-  {
-      // mixing two 64-bit values
-      uint64_t x = ndcs[A];
-      uint64_t y = ndcs[A + 1];
-
-      // mixing x with y, and y with x
-      x ^= rot(y, 13);
-      y ^= rot(x, 25);
-      x ^= rot(y, 27);
-
-      // replacing multiplication with additional bit manipulations for speed
-      x = (x << 5) | (x >> (64 - 5)); // left rotate by 5 bits
-      y = (y << 17) | (y >> (64 - 17)); // left rotate by 17 bits
-
-      // Storing back mixed values
-      ndcs[A] = y;
-      ndcs[A + 1] = x;
-  }
-  */
-
-  static inline void mix(const int A)
-  {
-      uint64_t x = ndcs[A];
-      uint64_t y = ndcs[A + 1];
-
-      x ^= y;
-      y ^= (x << 7) | (x >> (64 - 7));
-
-      ndcs[A] = x;
-      ndcs[A + 1] = y;
-  }
-
-
-  static inline void round( const uint64_t * m64, const uint8_t * m8, int len )
-  {
-    int index = 0;
-    int sindex = 0;
-    uint64_t counter = 0xfaccadaccad09997;
-    uint8_t counter8 = 137;
-
-    for( int Len = len >> 3; index < Len; index++) {
-      ndcs[sindex] += rot(m64[index] + index + counter + 1, 23);
-      counter += ~m64[index] + 1;
-      switch(sindex) {
-        case 1:
-          mix(0);
-          sindex++;
-          break;
-        case 3:
-          mix(2);
-          sindex++;
-          break;
-        case 5: 
-          mix(4);
-          sindex++;
-          break;
-        case 7:
-          mix(6);
-          sindex = 0;
-          break;
-        default:
-          sindex++;
-          break;
-      }
+    // Process 64-bit blocks
+    int num_blocks = len / 8;
+    for (int i = 0; i < num_blocks; i++) {
+        uint64_t block;
+        memcpy(&block, aligned_data + i * 8, 8);
+        
+        hash ^= block;
+        hash = rotl64(hash, 13);
+        hash = hash * PRIME_MULTIPLIER + 0x9E3779B97F4A7C15ULL;
     }
 
-    mix(1);
-    mix(3);
-    mix(5);
-
-    index <<= 3;
-    sindex = index&(STATEM);
-    for( ; index < len; index++) {
-      ndcs8[sindex] += rot8(m8[index] + index + counter8 + 1, 23);
-      counter8 += ~m8[index] + 1;
-      mix(index%STATE64M);
-      if ( sindex >= STATEM ) {
-        sindex = 0;
-      } else {
-        sindex++;
-      }
+    // Process leftover bytes
+    int offset = num_blocks * 8;
+    for (int i = 0; i < len - offset; i++) {
+        hash ^= aligned_data[offset + i];
+        hash = rotl64(hash, 5);
+        hash = hash * PRIME_MULTIPLIER + 0xBF58476D1CE4E5B9ULL;
     }
 
-    mix(0);
-    mix(1);
-    mix(2);
-    mix(3);
-    mix(4);
-    mix(5);
-    mix(6);
-  }
+    // Finalization
+    hash ^= len;
+    hash ^= (hash >> 33);
+    hash *= 0xff51afd7ed558ccdULL;
+    hash ^= (hash >> 33);
+    hash *= 0xc4ceb9fe1a85ec53ULL;
+    hash ^= (hash >> 33);
 
-  void newdisco_64 ( const void * key, int len, unsigned seed, void * out )
-  {
-    int tempLen = len;
-    if ( tempLen == 0 ) {
-      tempLen = 1;
-    }
-    alignas(uint64_t) uint8_t* tempBuf = new uint8_t[tempLen];
-    const uint8_t *key8Arr = (uint8_t *)key;
+    memcpy(out, &hash, sizeof(hash)); // copy the result into the output buffer
+}
 
-    alignas(uint64_t) const uint8_t seedbuf[16] = {0};
-    const uint8_t *seed8Arr = (uint8_t *)seedbuf;
-    const uint64_t *seed64Arr = (uint64_t *)seedbuf;
-    uint32_t *seed32Arr = (uint32_t *)seedbuf;
-
-    // the cali number from the Matrix (1999)
-    seed32Arr[0] = 0xc5550690;
-    seed32Arr[0] -= seed;
-    seed32Arr[1] = 1 + seed;
-    seed32Arr[2] = ~(1 - seed);
-    seed32Arr[3] = (1+seed) * 0xf00dacca;
-
-    // nothing up my sleeve
-    ndcs[0] = 0x123456789abcdef0;
-    ndcs[1] = 0x0fedcba987654321;
-    ndcs[2] = 0xaccadacca80081e5;
-    ndcs[3] = 0xf00baaf00f00baaa;
-    ndcs[4] = 0xbeefdeadbeefc0de;
-    ndcs[5] = 0xabad1deafaced00d;
-    ndcs[6] = 0xfaceb00cfacec0de;
-    ndcs[7] = 0xdeadc0dedeadbeef;
-
-    memcpy(tempBuf, key, len);
-    uint64_t* temp64 = reinterpret_cast<uint64_t*>(tempBuf);
-
-    round( temp64, key8Arr, len );
-    round( seed64Arr, seed8Arr, 16 );
-    round( ndcs, ndcs8, STATE );
-
-    // 512-bit internal state 256-bit output
-    uint64_t h[4] = {0}; // This will hold the final output
-
-    h[0] -= ndcs[2];
-    h[0] -= ndcs[3];
-    h[0] -= ndcs[6];
-    h[0] -= ndcs[7];
-
-    // full 256-bit output
-    /*
-      h[1] = ndcs[2] ^ ndcs[3] ^ ndcs[6] ^ ndcs[7];
-
-      h[2] -= ndcs[0];
-      h[2] -= ndcs[1];
-      h[2] -= ndcs[4];
-      h[2] -= ndcs[5];
-
-      h[3] = ndcs[0] ^ ndcs[1] ^ ndcs[4] ^ ndcs[5];
-    */
-
-    memcpy(out, h, sizeof(h)/4);  // divide by 4 as smhasher only uses 64 bits
-
-    delete[] tempBuf;
-  }
